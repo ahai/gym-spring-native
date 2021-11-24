df15dd6a2dceeef6e94737f3101617958b01af3a
ahai <seamorning@gmail.com>
2021-11-24 08:32:23 -0500

    first commit2

---
+++ src/main/java/com/example/demo100ms/Demo100msApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo100ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo100msApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo100msApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo100ms/Demo100msApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo100ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo100msApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
